67c8ea04f7649a2fc7e178fe0651d2819f7e523e
RuningMonk <wy781080032@126.com>
2018-12-18 13:44:35 +0800

    init this work

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+		<div class="container">
+			<button type="button" class="btn btn-default">123123</button>
+		</div>
+    <img id="img" src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+	mounted() {
+		$("#img").click(function(){
+			alert('1')
+		})
+	}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
